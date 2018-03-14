3c253482ce30e678dff8d9418c3143b52356e285
web-padawan <serguey.kulikov@gmail.com>
2018-03-14 11:23:08 +0200

    Add tests for overlay animations

--- test/test-suites.js
+++ test/test-suites.js
@@ -1,4 +1,6 @@
 window.VaadinOverlaySuites = [
   'vaadin-overlay.html',
-  'vaadin-overlay.html?wc-shadydom=true'
+  'vaadin-overlay.html?wc-shadydom=true',
+  'animate-overlay.html',
+  'animate-overlay.html?wc-shadydom=true'
 ];
