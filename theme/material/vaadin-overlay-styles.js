ff6cb081c15cb546d9b08d1f1947865bfa54d685
web-padawan <iamkulykov@gmail.com>
2021-02-18 15:14:18 +0200

    Replace dom-module with registerStyles

--- theme/material/vaadin-overlay-styles.js
+++ theme/material/vaadin-overlay-styles.js
@@ -1,12 +1,4 @@
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import '@vaadin/vaadin-material-styles/mixins/overlay.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 
-const $_documentContainer = html`<dom-module id="material-vaadin-overlay" theme-for="vaadin-overlay">
-  <template>
-    <style include="material-overlay">
-      /* stylelint-disable no-empty-source */
-    </style>
-  </template>
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
+registerStyles('vaadin-overlay', css``, { include: ['material-overlay'], moduleId: 'material-vaadin-overlay' });
